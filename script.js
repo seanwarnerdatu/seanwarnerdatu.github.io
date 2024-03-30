f508aeaefaae48a78da1d2a099654f0ffae78d23
Ahmyaaa <seandatu12@gmail.com>
2024-03-30 13:54:14 +0800

    new updated finals code

--- script.js
+++ script.js
@@ -17,3 +17,47 @@ function showScrollTopButton() {
         scrollTopButton.style.display = 'none';
     }
 }
+
+var slideIndex = 1; // Start with the first slide
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  
+  // If reached the end of slides, go back to the first slide
+  if (n > slides.length) { slideIndex = 1; }    
+  if (n < 1) { slideIndex = slides.length; }
+  
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  // Remove active class from all dots
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  // Display the current slide
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Add event listeners to the previous and next buttons
+document.querySelector('.prev').addEventListener('click', function() {
+  plusSlides(-1);
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+  plusSlides(1);
+});
