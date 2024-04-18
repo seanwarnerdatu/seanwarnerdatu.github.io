1405a68a8ffeb5f191f7c7f02cbf78fad6821924
Ahmyaaa <seandatu12@gmail.com>
2024-04-18 21:27:31 +0800

    Updated Portfolio

--- script.js
+++ script.js
@@ -63,7 +63,32 @@ document.querySelector('.next').addEventListener('click', function() {
 });
 
 
-document.getElementById('menu-icon').addEventListener('click', function() {
-  var navbarLinks = document.getElementById('navbar');
-  navbarLinks.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+  const popupOverlay = document.querySelector('.popup-overlay');
+  const closeButton = document.querySelector('.close-btn');
+
+  try {
+      // Check if popup has been displayed before
+      const popupDisplayed = localStorage.getItem('popupDisplayed');
+
+      if (!popupDisplayed) {
+          // Show the popup if it hasn't been displayed before
+          popupOverlay.style.display = 'flex';
+          localStorage.setItem('popupDisplayed', true);
+      }
+
+      function closePopup() {
+          popupOverlay.classList.add('fade-out');
+          setTimeout(function() {
+              popupOverlay.style.display = 'none';
+              popupOverlay.classList.remove('fade-out');
+          }, 500); // Adjust the time according to your fade animation duration
+      }
+
+      closeButton.addEventListener('click', function() {
+          closePopup();
+      });
+  } catch (error) {
+      console.error('Error accessing localStorage:', error);
+  }
 });
